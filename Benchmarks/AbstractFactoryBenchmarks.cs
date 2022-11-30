35153bc3e45ad5be30d2fb936b6121691f9df811
Zhdanov Roman <zhdanov.roma@ulh.ru>
2022-11-30 12:55:25 +0300

    Added benchmark project just for fun.

---
+++ Benchmarks/AbstractFactoryBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Benchmarks
+{
+    [RankColumn]
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class AbstractFactoryBenchmarks
+    {
+        [Benchmark]
+        public void RunGameStraightCode()
+        {
+            new TryOut.AbstractFactory.StraightCode.Game();
+        }
+
+        [Benchmark]
+        public void RunGameRefactoredCode()
+        {
+            new TryOut.AbstractFactory.RefactoredCode.Game();
+        }
+    }
+}
